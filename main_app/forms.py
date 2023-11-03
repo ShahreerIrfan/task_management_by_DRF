4f29b223e2442b3a022d0b2428c0c12aae0da524
unknown <mdshahreerirfan@gmail.com>
2023-11-03 23:18:42 +0600

    login registration done

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from .models import Task
 
 class TaskUpdateForm(forms.ModelForm):
@@ -6,13 +8,21 @@ class TaskUpdateForm(forms.ModelForm):
         model = Task
         fields = ['title', 'description', 'due_date', 'priority', 'photos', 'is_complete']
 
-from django import forms
-from .models import Task
-
 class TaskCreateForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'due_date', 'priority','photos']
 
 
-# Hi
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- main_app/views.py
+++ main_app/views.py
@@ -36,4 +36,33 @@ class TaskDetailView(DetailView):
     model = Task
     template_name = 'main_app/task_detail.html'
 
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from .forms import RegistrationForm, LoginForm
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your actual homepage URL
+    else:
+        form = RegistrationForm()
+    return render(request, 'main_app/register.html', {'form': form})
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your actual homepage URL
+    else:
+        form = LoginForm()
+    return render(request, 'main_app/login.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')  # Redirect to the login page after logging out
 
